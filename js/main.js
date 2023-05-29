5fd1ec4fe71edb9c240a9d941be49b3deb6c7ec6
Marek <sincereakram@gmail.com>
2023-05-29 10:06:11 +0300

    Nesigauna

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-import data from './data' 
+// import account from  "./data.js"
 
 const months = [sausis, vasaris, kovas, balandis, geguze, birzelis, liepa, rugpjutis, rugsejis, spalis, lapkritis, gruodis]
+console.log(months);
+
+const manoMetai = document.querySelector('h1')
+manoMetai.innerHTML = '2023'
+
+const balancas = 0;
+// account.map(element => balancas + element.income)
+
+const metųBalansas = document.querySelector('h2')
+metųBalansas.innerHTML = '2023'
+console.log(balancas);
